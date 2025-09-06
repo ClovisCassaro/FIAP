41e205eb0b7458e790b50730c31039e60c54e933
Clóvis Cassaro <clovis_c@hotmail.com>
2025-09-06 18:05:20 -0300

    Aula 8: Hot e Cold cache

---
+++ Aula8/Controllers/TimeSeriesController.cs
@@ -0,0 +1,19 @@
+﻿using dotnetCache.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetCache.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class TimeSeriesController(ITimeSeriesService timeSeriesService) : ControllerBase
+{
+    private readonly ITimeSeriesService _timeSeriesService = timeSeriesService;
+    [HttpGet]
+    public async Task<IActionResult> GetProduct()
+    {
+        var data = await _timeSeriesService.GetTimeSeries();
+        return Ok(data);
+    }
+
+}
